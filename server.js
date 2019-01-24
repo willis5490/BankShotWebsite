5126b85bb99fafa46c34688bcd153f4207424a73
William Stearns <wcstearns@yahoo.com>
2019-01-24 16:08:00 -0700

    blur

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post('/send-email', function(req, res) {
   let Name = JSON.stringify(req.body.name)
   let Message = JSON.stringify(req.body.message)
  
-  sgMail.setApiKey(process.env.keys);
+  sgMail.setApiKey('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');
  
 const msg = {
   to: "bankshotco@gmail.com",
